68a673fcba2e31d36b2ad3759332f53cc254ddfa
Dandi <dandi.061100@gmail.com>
2025-09-13 17:50:20 +0700

    chrore: change import url strategy

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:base_project/core/theme/app_theme.dart';
 import 'package:base_project/core/theme/cubit/theme_cubit.dart';
 import 'package:base_project/core/utils/app_utils.dart';
 import 'package:base_project/main_module.dart';
-import 'package:base_project/core/utils/url_strategy/setup_url_strategy_io.dart';
+import 'package:base_project/core/utils/url_strategy/setup_url_strategy.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
