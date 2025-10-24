0848c4008327d07f799c282892ba6d999ae95142
Dandi <dandi.061100@gmail.com>
2025-10-24 16:15:24 +0700

    fix(register): add appbar

--- lib/module/auth/presentation/pages/register/register_page.dart
+++ lib/module/auth/presentation/pages/register/register_page.dart
@@ -25,8 +25,9 @@ class _RegisterUIState extends State<RegisterUI> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       body: Padding(
-        padding: const EdgeInsets.all(32),
+        padding: const EdgeInsets.all(24),
         child: Column(
           spacing: 16,
           children: [
@@ -44,14 +45,6 @@ class _RegisterUIState extends State<RegisterUI> {
               onPressed: () {
                 AppRouteRedirect.alreadyLogin = true;
                 context.goNamed(AppRouteName.home);
-              },
-            ),
-            DesignButton(
-              type: DesignButtonType.outlined,
-              size: DesignButtonSize.large,
-              text: "Back",
-              onPressed: () {
-                context.pop();
               },
             ),
           ],
