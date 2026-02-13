cbba6cc80f5d1ee7e73f8fad3b215be20286d253
Dandi <dandi.061100@gmail.com>
2026-02-13 13:50:31 +0700

    refactor: remove unnecessary imports in brick module

--- bricks/module/__brick__/{{name.snakeCase()}}/{{name.snakeCase()}}_module.dart
+++ bricks/module/__brick__/{{name.snakeCase()}}/{{name.snakeCase()}}_module.dart
@@ -1,7 +1,3 @@
-import 'package:base_project/core/utils/app_utils.dart';
-import 'data/datasources/{{name.snakeCase()}}_local_datasource.dart';
-import 'data/datasources/{{name.snakeCase()}}_remote_datasource.dart';
-
 class {{name.pascalCase()}}Module {
   {{name.pascalCase()}}Module._();
 
