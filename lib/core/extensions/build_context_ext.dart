4852d78b59a63149dc746f6730840155b8160567
Dandi <dandi.061100@gmail.com>
2026-02-20 21:12:09 +0700

    chore: add build context ext

--- lib/core/extensions/build_context_ext.dart
+++ lib/core/extensions/build_context_ext.dart
@@ -11,6 +11,7 @@ extension BuildContextExt on BuildContext {
   double get screenWidth => MediaQuery.sizeOf(this).width;
   double get screenHeight => MediaQuery.sizeOf(this).height;
 
+  TextTheme get textTheme => Theme.of(this).textTheme;
   ColorScheme get colorScheme => Theme.of(this).colorScheme;
 
   bool get isMobileSize {
